3a194499a1560c1178ac08872217bf3d07d84d88
durangianka <63736409+durangianka@users.noreply.github.com>
2022-04-03 23:32:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     mapbox_key: 'pk.eyJ1IjoiZ2lhbmNhcmxvLWR1cmFudCIsImEiOiJjbDFmN291cG4wMjBuM2ltbHB5OWkxOGVkIn0.2_bXBeuswhUSwXs4BKmEFQ',
-    server: VERCEL_URL
+    server: "localhost:3000"
   }
 }
 
